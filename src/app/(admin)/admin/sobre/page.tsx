dfdb1fdb268e71cc9b603116660b54cd8b2a7ee1
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-06-28 00:30:19 -0300

    :sparkles: feat: Adicionar metadados à página "Sobre" para Admin e Professor
    - Incluídos metadados com título "Sobre" nas páginas de administração e professor, melhorando a SEO e a acessibilidade.
    - Atualizações realizadas nos componentes correspondentes para refletir as novas informações de metadados.

--- src/app/(admin)/admin/sobre/page.tsx
+++ src/app/(admin)/admin/sobre/page.tsx
@@ -1,4 +1,9 @@
 import AboutPage from "@/components/pages/about-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Sobre",
+};
 
 export default function AdminAboutPage() {
     return <AboutPage />;
